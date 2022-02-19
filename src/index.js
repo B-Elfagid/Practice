0cbe655727fef8a4563226ad4569d341283c9e70
Betty <betty_elfagid@yahoo.co.uk>
2022-02-19 20:40:36 +0000

    pass props to make the component dynamic

--- src/index.js
+++ src/index.js
@@ -2,25 +2,31 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
+const author = 'Colleen Hoover'
+const title = 'Reminders of him'
+const img = 'https://images-na.ssl-images-amazon.com/images/I/71rdsaOMvVL._AC_UL127_SR127,127_.jpg'
+
 function BookList(){
-  return <section> <Book /> <Book /> <Book /></section>
+  return(
+  <section className='booklist'> 
+     <Book job='developer' /> 
+  </section>) 
 }
 
-const Book = () => {
-  return <article>
-     <Image></Image>
-     <Title />
-     <Author />
+
+
+const Book = (props) => {
+  return(
+   <article className='book'>
+    <img src= {img}
+     alt="" />
+    <h1>{title}</h1> 
+    <h4>{author} </h4>
+    <p>{props.job}</p>
   </article>
+  )
 }
 
-const Image = () => (
-  <img src="https://images-na.ssl-images-amazon.com/images/I/71rdsaOMvVL._AC_UL127_SR127,127_.jpg" 
-  alt="" />
-)
-
-const Title = () => <h1>Reminders of him</h1> 
-const Author = () => <h2>Colleen Hoover</h2>
 
 ReactDom.render(<BookList />, document.getElementById('root'))
 
