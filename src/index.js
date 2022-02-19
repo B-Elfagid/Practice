5252ef3a46aaaa0242c3c149991c0acb975e6af2
Betty <betty_elfagid@yahoo.co.uk>
2022-02-19 14:44:30 +0000

    practiced nested components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+function Greeting(){
+  return (
+    <div>
+      <Person />
+     <h4>This is Betty and This is My first Component</h4>
+     </div>
+      )
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Person = () => {
+  return <h2>Betty Davide</h2>
+}
+// const Greeting = () => {
+//   return React.createElement('h1', {}, 'hello')
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(<Greeting />, document.getElementById('root'))
+
+
