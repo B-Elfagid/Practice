c621def247577b6c8b707d8c17b8b2931776848c
Betty <betty_elfagid@yahoo.co.uk>
2022-02-19 21:01:40 +0000

    destracturingprops

--- src/index.js
+++ src/index.js
@@ -2,27 +2,40 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
-const author = 'Colleen Hoover'
-const title = 'Reminders of him'
-const img = 'https://images-na.ssl-images-amazon.com/images/I/71rdsaOMvVL._AC_UL127_SR127,127_.jpg'
+// setup vars
+
+const firstBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/71rdsaOMvVL._AC_UL127_SR127,127_.jpg',
+  title: 'Reminders of him',
+  author: 'Colleen Hoover'
+}
+
+const secondBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/51Zu0ZwT0jL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+  title: 'It Ends With Us',
+  author: 'Colleen Hoover'
+}
+
+
 
 function BookList(){
   return(
   <section className='booklist'> 
-     <Book job='developer' /> 
+     <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} /> 
+     <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
   </section>) 
 }
 
 
 
 const Book = (props) => {
+  const { img, title, author } = props;
   return(
    <article className='book'>
     <img src= {img}
      alt="" />
     <h1>{title}</h1> 
     <h4>{author} </h4>
-    <p>{props.job}</p>
   </article>
   )
 }
